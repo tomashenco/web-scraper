732d2d0e0554df78229e7c0bd2aa8ad4c152f570
tomashenco <dobrzycki.tomasz@gmail.com>
2016-01-21 19:40:55 +0000

    Working version, needs tests

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import logging
 import json
-from tools import fetch_html, fetch_links, fetch_title, fetch_tree, fetch_price, fetch_description
+from collections import OrderedDict
+from tools import fetch_html, fetch_links, fetch_title, fetch_tree, fetch_price, fetch_description, sum_price
 
 
-def scrap():
+def scrap(url):
     """ Gets page and summarises all product information
     :param url: page address
     :return:
@@ -13,19 +14,27 @@ def scrap():
     logger = logging.getLogger('log')
     ch = logging.StreamHandler()
     ch.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    url = 'http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html'
+    # Fetch main page
     _, main_content = fetch_html(url)
+    # Fetch links to all products on page
     results = fetch_links(main_content)
+
+    # Create a list of products
+    products = []
     for page in results:
         size, content = fetch_html(page)
         tree = fetch_tree(content)
-        print fetch_title(tree)
-        print fetch_price(tree)
-        print fetch_description(tree)
+        product = OrderedDict([('title', fetch_title(tree)),
+                               ('size',  size),
+                               ('unit_price', fetch_price(tree)),
+                               ('description',  fetch_description(tree))])
+        products.append(product)
 
+    # Show the result
+    print json.dumps({'results': products, 'totals': sum_price(products)}, indent=2)
 
-scrap()
+scrap('http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html')
--- tools.py
+++ tools.py
@@ -6,6 +6,12 @@ logger = logging.getLogger('log')
 
 
 def fetch_html(url):
+    """
+    Gets html and size of it
+    :param url: Link to page
+    :type url: string
+    :return: size of website, content of website
+    """
     try:
         results = requests.get(url)
         logger.info('Website found successfully.')
@@ -13,22 +19,48 @@ def fetch_html(url):
         logger.error('Failed to find the page: [ %s ]' % url)
         return None, None
 
-    page_size = int(results.headers.get('Content-Length')) / 1024.0
+    page_size = '%.1fkb' % (int(results.headers.get('Content-Length')) / 1024.0)
 
     return page_size, results.content
 
 
-def fetch_tree(content):
-    return html.fromstring(content)
-
-
 def fetch_links(content):
+    """
+    Gets all product links
+    :param content: Content of page
+    :type content: requests content
+    :return: links to products
+    """
     tree = fetch_tree(content)
-    links = tree.xpath('//div[@class="productInfo"]/h3/a')
+    try:
+        links = tree.xpath('//div[@class="productInfo"]/h3/a')
+    except AttributeError:
+        logger.error('Broken tree')
+        return []
+
     return [link.get('href') for link in links]
 
 
+def fetch_tree(content):
+    """
+    Gets lxml tree
+    :param content: Content of page
+    :type content: requests content
+    :return: lxml tree
+    """
+    try:
+        return html.fromstring(content)
+    except TypeError:
+        logger.error('Broken website content')
+        return None
+
+
 def fetch_title(tree):
+    """
+    Gets product title
+    :param tree: lxml tree
+    :return: product title
+    """
     try:
         (title, ) = tree.xpath('//div[@class="productTitleDescriptionContainer"]/h1/text()')
     except ValueError:
@@ -39,6 +71,11 @@ def fetch_title(tree):
 
 
 def fetch_price(tree):
+    """
+    Gets product price
+    :param tree: lxml tree
+    :return: product price
+    """
     try:
         (price, _) = tree.xpath('//div[@class="productSummary"]//p[@class="pricePerUnit"]/text()')
     except ValueError:
@@ -46,10 +83,24 @@ def fetch_price(tree):
         return None
 
     _, real_price = price.split(u'\xa3')
-    return str(real_price)
+    return float(real_price)
 
 
 def fetch_description(tree):
+    """
+    Gets product description
+    :param tree: lxml tree
+    :return: product description
+    """
     description = tree.xpath('//div[@class="productText"]/p/text()')[0]
 
     return description
+
+
+def sum_price(products):
+    """
+    Sums up total price of products
+    :param products: list of products
+    :return: total price
+    """
+    return round(sum(product['unit_price'] for product in products), 2)
