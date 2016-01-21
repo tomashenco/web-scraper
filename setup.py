b453de4514e2a546b2306b5d935b67ee7ff229be
tomashenco <dobrzycki.tomasz@gmail.com>
2016-01-21 20:26:53 +0000

    Setup

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     description="Sainsbury's website scraper. Console application that will find all products and create a summary.",
     long_description=open('README.md').read(),
     url='https://github.com/tomashenco/web-scraper',
-    packages=['web-scraper'],
     install_requires=['requests>=2.8.1',
                       'lxml>=3.4.4'],
     classifiers=[
