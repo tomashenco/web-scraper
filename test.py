f2925ff51a69dbe55a3ca8966f355a1f251d561f
tomashenco <dobrzycki.tomasz@gmail.com>
2016-01-22 10:41:59 +0000

    Last commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import unittest
+from tools import fetch_html, fetch_links, fetch_title, fetch_tree, fetch_price, fetch_description, sum_price
+
+
+class TestToolsFunctions(unittest.TestCase):
+    def test_if_function_fetch_price_gives_number(self):
+        self.assertTrue()
